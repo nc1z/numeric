d52add183f4bfd0486c30cd82419de1a5ec20e26
nc1z <ncxwork@gmail.com>
2024-01-13 22:08:44 +0800

    feat: configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: ['.env.development.local', '.env.development'],
+            isGlobal: true,
+        }),
+        AuthModule,
+    ],
 })
 export class AppModule {}
