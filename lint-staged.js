6275269d7a97eda853f5f60b67781322e9a2764f
nc1z <ncxwork@gmail.com>
2024-01-13 22:17:24 +0800

    chore: update lint-staged

--- lint-staged.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    '*.{js,ts}': [
-        'eslint "{src,apps,libs,test}/**/*.ts" --fix --max-warnings=0',
-        () => 'tsc-files --noEmit',
-    ],
-    '*.{js,ts,json}': ['prettier --write'],
-};
