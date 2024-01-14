995ddd0c1bc8b833f9d04140860052fa9b85c473
nc1z <ncxwork@gmail.com>
2024-01-14 16:10:32 +0800

    feat: added prisma ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
-            envFilePath: ['.env.development.local', '.env.development'],
+            envFilePath: ['.env'],
             isGlobal: true,
         }),
         AuthModule,
         UserModule,
+        PrismaModule,
     ],
+    providers: [PrismaService],
 })
 export class AppModule {}
