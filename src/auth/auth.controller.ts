dd7c6af36a46a76cafbc3b1eef3420ffb6ba0ffe
nc1z <ncxwork@gmail.com>
2024-01-31 22:26:53 +0800

    feat: validation for auth endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('login')
-    login() {
-        return this.authService.login();
+    login(@Body() dto: AuthDto) {
+        return this.authService.login(dto);
     }
 
     @Post('register')
-    register() {
-        return this.authService.register();
+    register(@Body() dto: AuthDto) {
+        return this.authService.register(dto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
 
-    login() {
-        return { data: 'login test' };
+    login(dto: AuthDto) {
+        return { data: dto };
     }
 
-    register() {
-        return { data: 'register test' };
+    register(dto: AuthDto) {
+        return { data: dto };
     }
 }
