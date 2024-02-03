27d1b1ccd4bd94b622d7465aa2f07d3406ac2f4a
nc1z <ncxwork@gmail.com>
2024-02-03 14:47:19 +0800

    feat: registration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
-
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
@@ -10,7 +11,29 @@ export class AuthService {
         return { data: dto };
     }
 
-    register(dto: AuthDto) {
-        return { data: dto };
+    async register(dto: AuthDto) {
+        const { email, password } = dto;
+        const hash = await argon.hash(password);
+
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email,
+                    hash,
+                },
+            });
+            delete user.hash;
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                switch (error.code) {
+                    case 'P2002':
+                        throw new ForbiddenException(
+                            'Credentials already in use',
+                        );
+                }
+            }
+            throw error;
+        }
     }
 }
